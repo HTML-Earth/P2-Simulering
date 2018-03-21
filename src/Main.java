b0da92440fb3eec5e6af9445f774fba19a9aea68
Rynke <37619846+Rynke@users.noreply.github.com>
2018-03-21 12:17:17 +0100

    Test

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main
     {
         System.out.println("Hello world!");
         System.out.println("xd");
-
+        System.out.println("Rynke was here");
         System.out.println("lmao");
     }
 }

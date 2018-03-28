08c7cf10eb7a1e3adfa9c58ec8909db3626b0113
Rynke <37619846+Rynke@users.noreply.github.com>
2018-03-28 10:48:39 +0200

    WindowTest
    xd

--- src/dk/aau/cs/a310a/GUI.java
+++ src/dk/aau/cs/a310a/GUI.java
@@ -1,6 +1,22 @@
 package dk.aau.cs.a310a;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 public class GUI extends Application {
+
+    public void start(Stage stage) {
+        stage.setTitle("WindowTest");
+
+        HBox root = new HBox();
+
+        Scene scene = new Scene(root, 800, 400);
+        stage.setScene(scene);
+
+        stage.show();
+
+    }
+
 }

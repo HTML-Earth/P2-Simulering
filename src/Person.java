01f628d3a1c2fbf63c826814f5c0010fa981b03f
GarridoY <33898925+GarridoY@users.noreply.github.com>
2018-03-28 10:10:28 +0200

    Added class: Person

---
+++ src/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    private boolean isSusceptible;
+    private boolean isInfected;
+    private boolean isRecovered;
+
+    public Person(boolean isSusceptible, boolean isInfected, boolean isRecovered) {
+        this.isSusceptible = isSusceptible;
+        this.isInfected = isInfected;
+        this.isRecovered = isRecovered;
+    }
+}

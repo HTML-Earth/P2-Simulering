2ddef841d966b811d3fc3b4f04139e1302155953
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2018-03-28 10:50:09 +0200

    move main to gui

--- src/dk/aau/cs/a310a/GUI.java
+++ src/dk/aau/cs/a310a/GUI.java
@@ -5,7 +5,26 @@ import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
+import java.util.Random;
+
 public class GUI extends Application {
+
+    public static void main(String[] args)
+    {
+        launch(args);
+        Random rand = new Random();
+        System.out.println("Hello world!");
+        System.out.println("xd");
+        System.out.println("Rynke was here");
+        System.out.println("lmao");
+        System.out.println("hah");
+        System.out.println("Hello flu!");
+
+        for(int i = 0; i < 100; i ++) {
+            int randInt = rand.nextInt();
+            Person person = new Person(randInt, Person.health.isSusceptible);
+        }
+    }
 
     public void start(Stage stage) {
         stage.setTitle("WindowTest");

de59262cf447ab6b52302688e51d0acecb114345
GarridoY <33898925+GarridoY@users.noreply.github.com>
2018-04-03 11:17:31 +0200

    Added Test

--- src/dk/aau/cs/a310a/Influenza.java
+++ src/dk/aau/cs/a310a/Influenza.java
@@ -4,7 +4,7 @@ public class Influenza {
     private int baseSpread;  //Beta
     private int amountCured; //Gamma
 
-    private enum influenzaType {A, B}
+    public enum influenzaType {A, B}
     private influenzaType type;
 
     public Influenza(influenzaType type) {
@@ -20,12 +20,20 @@ public class Influenza {
         }
     }
 
+    public int getBaseSpread() {
+        return baseSpread;
+    }
+
+    public int getAmountCured() {
+        return amountCured;
+    }
+
     public void infectPerson(Person person) {
         person.setCurrentHealth(Person.health.Infected);
     }
 
-    public double calculateR0(int spread, int amountCured) {
-        double isEpidemic = (double)spread / amountCured; //R0
+    public double calculateR0(int beta, int gamma) {
+        double isEpidemic = (double)beta / gamma; //R0
 
         if(isEpidemic >= 1)
             System.out.println("There is a chance of an epidemic.");
--- src/dk/aau/cs/a310a/TestProgram.java
+++ src/dk/aau/cs/a310a/TestProgram.java
@@ -1,13 +1,27 @@
 package dk.aau.cs.a310a;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestProgram {
-    /*
+
+    //Test om R0 beregningen bliver foretaget korrekt
     @Test
-    public void isSusceptible() {
-        assertEquals(2, 2);
+    public void isEpidemic01() {
+        Influenza influenzaA = new Influenza(Influenza.influenzaType.A);
+        double R0 = influenzaA.calculateR0(influenzaA.getBaseSpread(),influenzaA.getAmountCured());
+        assertEquals(2, R0);
     }
-    */
+
+    //Test om person/objektet bliver inficeret
+    @Test
+    public void infectPerson01() {
+        Influenza influenzaA = new Influenza(Influenza.influenzaType.A);
+        Person person = new Person(25, Person.health.Susceptible);
+
+        influenzaA.infectPerson(person);
+        assertEquals(Person.health.Infected, person.getCurrentHealth());
+    }
+
 }

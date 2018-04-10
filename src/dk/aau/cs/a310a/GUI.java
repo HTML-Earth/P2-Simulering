d1155d0d633a06cfb1374e25738bd58e59cc9ed6
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2018-04-10 14:29:07 +0200

    pls no draw dead ppl destination

--- src/dk/aau/cs/a310a/GUI.java
+++ src/dk/aau/cs/a310a/GUI.java
@@ -340,7 +340,8 @@ public class GUI extends Application {
 
                     //Tegn alle personer og print deres info
                     for (Person p : sim.getPeople()) {
-                        bob.drawDestination(p.getPosition(), p.getDestination(), gc);
+                        if (p.getCurrentHealth() != Person.health.Dead)
+                            bob.drawDestination(p.getPosition(), p.getDestination(), gc);
                         bob.drawPerson(p.getPosition(),p.getCurrentHealth(),gc);
                         personData.setText(personData.getText() + "\n " + p);
                         stringSusceptible.setText("Susceptibles: " + sim.healthCount(Person.health.Susceptible));

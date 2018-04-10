29ea10a8d44d346d6320d4aab2e226d4e95e76f3
Simon Mathiasen <33898979+smath17@users.noreply.github.com>
2018-04-10 14:26:32 +0200

    Style method til grid

--- src/dk/aau/cs/a310a/Styler.java
+++ src/dk/aau/cs/a310a/Styler.java
@@ -1,10 +1,16 @@
 package dk.aau.cs.a310a;
 
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 public class Styler {
     public void StyleLabel(Label x) {
         x.getStylesheets().add(getClass().getResource("LabelStyle.css").toExternalForm());
         x.getStyleClass().add("label");
+    }
+
+    public void StyleGrid(GridPane y) {
+        y.getStylesheets().add(getClass().getResource("LabelStyle.css").toExternalForm());
+        y.getStyleClass().add("grid");
     }
 }

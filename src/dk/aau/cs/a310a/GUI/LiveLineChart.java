e37d711a30ca075383e3882e2450f817c8f3b839
Peterm95 <33749176+Peterm95@users.noreply.github.com>
2018-04-20 14:53:13 +0200

    double -> int chart

--- src/dk/aau/cs/a310a/GUI/LiveLineChart.java
+++ src/dk/aau/cs/a310a/GUI/LiveLineChart.java
@@ -12,15 +12,16 @@ import javafx.scene.chart.XYChart;
 
 public class LiveLineChart {
     LineChart lineChart;
-    ObservableList<XYChart.Series<String, Double>> chartData;
-    XYChart.Series<String, Double> sSeries;
-    XYChart.Series<String, Double> iSeries;
-    XYChart.Series<String, Double> rSeries;
+    ObservableList<XYChart.Series<Integer, Integer>> chartData;
+    XYChart.Series<Integer, Integer> sSeries;
+    XYChart.Series<Integer, Integer> iSeries;
+    XYChart.Series<Integer, Integer> rSeries;
     public final int ticksPerPoint = 144;
 
     public LineChart createLineChart() {
-        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis xAxis = new NumberAxis();
         NumberAxis yAxis = new NumberAxis();
+        //yAxis.setForceZeroInRange(false)
 
         lineChart = new LineChart(xAxis, yAxis);
         chartData = getChartData();
@@ -38,21 +39,22 @@ public class LiveLineChart {
     }
 
     public void updateLineChart() {
+
         Integer tick = Simulator.clock.getGraphTime();
         int susceptible = Simulator.theSimulator.healthCount(Person.health.Susceptible);
         int infected = Simulator.theSimulator.healthCount(Person.health.Infected);
         int recovered = Simulator.theSimulator.healthCount(Person.health.Recovered);
-        sSeries.getData().add(new XYChart.Data(tick.toString(), susceptible));
-        iSeries.getData().add(new XYChart.Data(tick.toString(), infected));
-        rSeries.getData().add(new XYChart.Data(tick.toString(), recovered));
+        sSeries.getData().add(new XYChart.Data(tick, susceptible));
+        iSeries.getData().add(new XYChart.Data(tick, infected));
+        rSeries.getData().add(new XYChart.Data(tick, recovered));
     }
 
-    private ObservableList<XYChart.Series<String, Double>> getChartData() {
-        ObservableList<XYChart.Series<String, Double>> answer = FXCollections
+    private ObservableList<XYChart.Series<Integer, Integer>> getChartData() {
+        ObservableList<XYChart.Series<Integer, Integer>> answer = FXCollections
                 .observableArrayList();
-        sSeries = new XYChart.Series<String, Double>();
-        iSeries = new XYChart.Series<String, Double>();
-        rSeries = new XYChart.Series<String, Double>();
+        sSeries = new XYChart.Series<Integer, Integer>();
+        iSeries = new XYChart.Series<Integer, Integer>();
+        rSeries = new XYChart.Series<Integer, Integer>();
         sSeries.setName("Susceptible");
         iSeries.setName("Infected");
         rSeries.setName("Recovered");

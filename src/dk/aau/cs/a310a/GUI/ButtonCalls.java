ced0703d13365a8f90bf7e99962225e1310b93e9
Rynke <37619846+Rynke@users.noreply.github.com>
2018-05-24 09:49:52 +0200

    errors til textfield

--- src/dk/aau/cs/a310a/GUI/ButtonCalls.java
+++ src/dk/aau/cs/a310a/GUI/ButtonCalls.java
@@ -1,14 +1,12 @@
 package dk.aau.cs.a310a.GUI;
 
 import dk.aau.cs.a310a.Simulation.Simulator;
-
 import javafx.scene.chart.LineChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.BoxBlur;
 import javafx.scene.effect.Effect;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -18,7 +16,7 @@ import javafx.stage.Stage;
 
 public class ButtonCalls {
 
-    void removeLabels (Button runButton, StackPane menu, Label appliedLabel) {
+    void removeLabels(Button runButton, StackPane menu, Label appliedLabel) {
         runButton.setDisable(true);
 
         if (menu.getChildren().contains(appliedLabel)) {
@@ -75,133 +73,141 @@ public class ButtonCalls {
             double risk = 0;
             int range = 0;
             double death = 0;
-            int maxDayRecover = 0;
+            int maxDayRecover = 1;
             int minDayRecover = 0;
             double moving = 0;
 
             //if numberTextField is empty initialize variable to 0
 
-            if (!peopleAmount.getText().equals("")){
-                people = Integer.parseInt(peopleAmount.getText());
-            }
+            try {
 
-            if (!infectedAmount.getText().equals("")){
-                infected = Integer.parseInt(infectedAmount.getText());
-            }
-
-            if (!vaccinePercent.getText().equals("")){
-                vaccinatedPercent = Integer.parseInt(vaccinePercent.getText());
-            }
-
-            if  (!sanitizerPercent.getText().equals("")){
-                handsanitizedPercent = Integer.parseInt(sanitizerPercent.getText());
-            }
-
-            if (!stayHomePercent.getText().equals("")){
-                staysHomePercent = Integer.parseInt(stayHomePercent.getText());
-            }
-
-            if (!coverMouthPercent.getText().equals("")){
-                coverCoughPercent = Integer.parseInt(coverMouthPercent.getText());
-            }
-
-            if (!infectionRisk.getText().equals("")){
-                risk = Double.parseDouble(infectionRisk.getText());
-            }
-            if (!infectionRange.getText().equals("")){
-                range = Integer.parseInt(infectionRange.getText());
-            }
-            if (!deathRisk.getText().equals("")){
-                death = Double.parseDouble(deathRisk.getText());
-            }
-            if (!maxDaysbfRecovered.getText().equals("")){
-                maxDayRecover = Integer.parseInt(maxDaysbfRecovered.getText());
-            }
-            if (!minDaysbfRecovered.getText().equals("")){
-                minDayRecover = Integer.parseInt(minDaysbfRecovered.getText());
-            }
-            if (!movingRisk.getText().equals("")){
-                moving = Double.parseDouble(movingRisk.getText());
-            }
-
-
-            //Alertbox hvis procenterne er over 100 eller under 0
-            if (!((0 <= vaccinatedPercent && vaccinatedPercent <= 100) && (0 <= handsanitizedPercent && handsanitizedPercent <= 100) && (0 <= staysHomePercent && staysHomePercent <= 100) && (0 <= coverCoughPercent && coverCoughPercent <= 100))) {
-                GUI.theGUI.displayMessage(Alert.AlertType.ERROR, "ERROR", "Invalid percentage", "Percentage must be between 0 - 100");
-                return;
-            }
-
-
-            if (people > 0 && infected > 0 && people <= 1000 && infected <= people) {
-                runButton.setDisable(false);
-                Simulator.theSimulator.clearSimulation();
-
-                //Graf med statistikker
-                gui.lineChart = new LiveLineChart();
-                LineChart chart = gui.lineChart.createLineChart(people);
-                styler.StyleChart(chart);
-
-                //Graf og canvas tilføjes til main panel
-                mainPanel.getChildren().set(0, chart);
-
-                //Opretter personer og sygdom
-                Simulator.theSimulator.initialiseSimulation(people, infected, risk, range, death, minDayRecover, maxDayRecover, moving);
-                //sætter personer til at være vaccineret
-                Simulator.theSimulator.vaccinatePeople(vaccinatedPercent, infected);
-                Simulator.theSimulator.handsanitizePeople(handsanitizedPercent);
-                Simulator.theSimulator.coverCoughPeople(coverCoughPercent);
-                Simulator.theSimulator.stayHomePeople(staysHomePercent);
-
-                runButton.setText("Start");
-                bob.drawBackground();
-                menu.getChildren().removeAll(tooBigPopulationLabel, population0Label);
-                if (menu.getChildren().contains(appliedLabel)) {
-                    menu.getChildren().remove(appliedLabel);
+                if (!peopleAmount.getText().equals("")) {
+                    people = Integer.parseInt(peopleAmount.getText());
                 }
-                if (menu.getChildren().contains(infectedOverPopLabel)) {
-                    menu.getChildren().remove(infectedOverPopLabel);
+
+                if (!infectedAmount.getText().equals("")) {
+                    infected = Integer.parseInt(infectedAmount.getText());
                 }
-                menu.getChildren().add(appliedLabel);
 
-            } else if (infected > 0 && people > 1000) {
-                if (menu.getChildren().contains(tooBigPopulationLabel)) {
-                    menu.getChildren().remove(tooBigPopulationLabel);
+                if (!vaccinePercent.getText().equals("")) {
+                    vaccinatedPercent = Integer.parseInt(vaccinePercent.getText());
                 }
-                menu.getChildren().remove(population0Label);
-                menu.getChildren().add(tooBigPopulationLabel);
 
-                removeLabels(runButton, menu, appliedLabel);
+                if (!sanitizerPercent.getText().equals("")) {
+                    handsanitizedPercent = Integer.parseInt(sanitizerPercent.getText());
+                }
 
-                runButton.setDisable(true);
-            } else if (infected > people) {
-                if (menu.getChildren().contains(population0Label)) {
+                if (!stayHomePercent.getText().equals("")) {
+                    staysHomePercent = Integer.parseInt(stayHomePercent.getText());
+                }
+
+                if (!coverMouthPercent.getText().equals("")) {
+                    coverCoughPercent = Integer.parseInt(coverMouthPercent.getText());
+                }
+
+                if (!infectionRisk.getText().equals("")) {
+                    risk = Double.parseDouble(infectionRisk.getText());
+                }
+                if (!infectionRange.getText().equals("")) {
+                    range = Integer.parseInt(infectionRange.getText());
+                }
+                if (!deathRisk.getText().equals("")) {
+                    death = Double.parseDouble(deathRisk.getText());
+                }
+                if (!maxDaysbfRecovered.getText().equals("")) {
+                    maxDayRecover = Integer.parseInt(maxDaysbfRecovered.getText());
+                }
+                if (!minDaysbfRecovered.getText().equals("")) {
+                    minDayRecover = Integer.parseInt(minDaysbfRecovered.getText());
+                }
+                if (!movingRisk.getText().equals("")) {
+                    moving = Double.parseDouble(movingRisk.getText());
+                }
+
+
+                //Alertbox hvis procenterne er over 100 eller under 0
+                if (!((0 <= vaccinatedPercent && vaccinatedPercent <= 100) && (0 <= handsanitizedPercent && handsanitizedPercent <= 100) && (0 <= staysHomePercent && staysHomePercent <= 100) && (0 <= coverCoughPercent && coverCoughPercent <= 100))) {
+                    GUI.theGUI.displayMessage(Alert.AlertType.ERROR, "ERROR", "Invalid percentage", "Percentage must be between 0 - 100");
+                    return;
+                }
+
+
+                if (people > 0 && infected > 0 && people <= 1000 && infected <= people) {
+                    runButton.setDisable(false);
+                    Simulator.theSimulator.clearSimulation();
+
+                    //Graf med statistikker
+                    gui.lineChart = new LiveLineChart();
+                    LineChart chart = gui.lineChart.createLineChart(people);
+                    styler.StyleChart(chart);
+
+                    //Graf og canvas tilføjes til main panel
+                    mainPanel.getChildren().set(0, chart);
+
+                    //Opretter personer og sygdom
+                    Simulator.theSimulator.initialiseSimulation(people, infected, risk, range, death, minDayRecover, maxDayRecover, moving);
+                    //sætter personer til at være vaccineret
+                    Simulator.theSimulator.vaccinatePeople(vaccinatedPercent, infected);
+                    Simulator.theSimulator.handsanitizePeople(handsanitizedPercent);
+                    Simulator.theSimulator.coverCoughPeople(coverCoughPercent);
+                    Simulator.theSimulator.stayHomePeople(staysHomePercent);
+
+                    runButton.setText("Start");
+                    bob.drawBackground();
+                    menu.getChildren().removeAll(tooBigPopulationLabel, population0Label);
+                    if (menu.getChildren().contains(appliedLabel)) {
+                        menu.getChildren().remove(appliedLabel);
+                    }
+                    if (menu.getChildren().contains(infectedOverPopLabel)) {
+                        menu.getChildren().remove(infectedOverPopLabel);
+                    }
+                    menu.getChildren().add(appliedLabel);
+
+                } else if (infected > 0 && people > 1000) {
+                    if (menu.getChildren().contains(tooBigPopulationLabel)) {
+                        menu.getChildren().remove(tooBigPopulationLabel);
+                    }
                     menu.getChildren().remove(population0Label);
-                }
-                if (menu.getChildren().contains(appliedLabel)) {
-                    menu.getChildren().remove(appliedLabel);
-                }
-                if (menu.getChildren().contains(tooBigPopulationLabel)) {
+                    menu.getChildren().add(tooBigPopulationLabel);
+
+                    removeLabels(runButton, menu, appliedLabel);
+
+                    runButton.setDisable(true);
+                } else if (infected > people) {
+                    if (menu.getChildren().contains(population0Label)) {
+                        menu.getChildren().remove(population0Label);
+                    }
+                    if (menu.getChildren().contains(appliedLabel)) {
+                        menu.getChildren().remove(appliedLabel);
+                    }
+                    if (menu.getChildren().contains(tooBigPopulationLabel)) {
+                        menu.getChildren().remove(tooBigPopulationLabel);
+                    }
+                    if (menu.getChildren().contains(infectedOverPopLabel)) {
+                        menu.getChildren().remove(infectedOverPopLabel);
+                    }
+                    menu.getChildren().add(infectedOverPopLabel);
+
+                    runButton.setDisable(true);
+                } else {
+                    if (menu.getChildren().contains(population0Label)) {
+                        menu.getChildren().remove(population0Label);
+                    }
+                    if (menu.getChildren().contains(infectedOverPopLabel)) {
+                        menu.getChildren().remove(infectedOverPopLabel);
+                    }
                     menu.getChildren().remove(tooBigPopulationLabel);
-                }
-                if (menu.getChildren().contains(infectedOverPopLabel)) {
-                    menu.getChildren().remove(infectedOverPopLabel);
-                }
-                menu.getChildren().add(infectedOverPopLabel);
+                    menu.getChildren().add(population0Label);
 
+                    removeLabels(runButton, menu, appliedLabel);
+                }
+
+            } catch (RuntimeException e) {
                 runButton.setDisable(true);
+                System.out.println(e);
+                GUI.theGUI.displayMessage(Alert.AlertType.ERROR, "ERROR", "Invalid Variables", "Population, range, days and fields with percent symbols \nmust be integers. ");
             }
-            else {
-                if (menu.getChildren().contains(population0Label)) {
-                    menu.getChildren().remove(population0Label);
-                }
-                if (menu.getChildren().contains(infectedOverPopLabel)) {
-                    menu.getChildren().remove(infectedOverPopLabel);
-                }
-                menu.getChildren().remove(tooBigPopulationLabel);
-                menu.getChildren().add(population0Label);
 
-                removeLabels(runButton, menu, appliedLabel);
-            }
         });
 
     }
@@ -223,8 +229,7 @@ public class ButtonCalls {
                 Simulator.theSimulator.pauseSimulation();
                 GUI.theGUI.pausePlaySim.setText("\u25B6");
                 GUI.theGUI.pausePlaySim.setFont(Font.font(20));
-            }
-            else {
+            } else {
                 Simulator.theSimulator.startSimulation();
                 GUI.theGUI.pausePlaySim.setText("\u23F8");
                 GUI.theGUI.pausePlaySim.setFont(Font.font(14));
@@ -232,11 +237,11 @@ public class ButtonCalls {
         });
     }
 
-    void stopSim () {
+    void stopSim() {
         GUI.theGUI.stopSim.setOnMouseClicked(event -> Simulator.theSimulator.stopSimulation());
     }
 
-    void printSim (Stage stage) {
+    void printSim(Stage stage) {
         GUI.theGUI.printSim.setOnMouseClicked(event -> GUI.theGUI.exportFileDialog(stage));
     }
 
